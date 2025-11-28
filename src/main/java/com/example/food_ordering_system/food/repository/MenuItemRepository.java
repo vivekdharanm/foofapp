e7cd98fed7f24cf3ffd1d34a19002cffa9b551e0
Vivekdharan Muthupandi <vivekmuthupandi@gmail.com>
2025-11-28 10:19:12 +0530

    Update MenuItemRepository.java

--- src/main/java/com/example/food_ordering_system/food/repository/MenuItemRepository.java
+++ src/main/java/com/example/food_ordering_system/food/repository/MenuItemRepository.java
@@ -1,5 +1,7 @@
 package com.example.food_ordering_system.food.repository;
 
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.food_ordering_system.food.model.MenuItem;
 
